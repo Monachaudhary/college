3b419dd1087aad7ac1e0753493222241836689ba
Manu Chaudhary <96776726+Monachaudhary@users.noreply.github.com>
2024-09-14 12:23:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,52 +19,6 @@ document.addEventListener('DOMContentLoaded', function() {
     hideSidebarButton.addEventListener('click', hideSidebar);
 });
 
-<<<<<<< HEAD
-// function sendmail(event){
-//     event.preventDefault();
-//     var params ={
-=======
-
-
-
-function sendmail(){
-    var params ={
->>>>>>> cf75b1b53a250454363bf645f0e9d5127eb4a5b5
-      
-//         message : document.getElementById("message").value,
-//         email_id : document.getElementById("email").value,
-//         services : document.getElementById("services").value,
-//         name : document.getElementById("name").value,
-//     };
-//     emailjs.send("service_b52pvoc","template_lhu5kvc", params)
-//     .then(
-//         res =>{
-//             document.getElementById("name").value = "";
-//             document.getElementById("message").value = "";
-//             document.getElementById("email_id").value = "";
-//             document.getElementById("services").value = "";
-//             console.log(res);
-//             showSuccessMessage("Your message has been sent successfully!");
-//         }
-//     )
-//     .catch((err)=> console.log("there is error"));
-// }
-// document.getElementById("contactForm").addEventListener("submit", sendmail);
-// function showSuccessMessage(message) {
-//     var successMessageDiv = document.createElement("div");
-//     successMessageDiv.className = "success-message";
-//     successMessageDiv.innerHTML = `<p>${message}</p>`;
-
-//     // Append the success message to the form or any container
-//     var formContainer = document.querySelector(body);
-//     formContainer.appendChild(successMessageDiv);
-
-//     // Optionally, you can hide the message after a few seconds
-//     setTimeout(() => {
-//         successMessageDiv.remove();
-//     }, 5000); // Hides after 5 seconds
-// }
-
 function sendmail(event) {
     event.preventDefault();
     
