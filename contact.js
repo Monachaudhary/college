0dd9609b4d9c0f024089488ab38f8e28fb2034e0
Manu Chaudhary <96776726+Monachaudhary@users.noreply.github.com>
2024-09-14 12:41:40 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const sidebar = document.querySelector('.sidebar');
     const menuButton = document.querySelector('.menu-button');
@@ -50,21 +49,3 @@ function sendmail(event) {
 }
 
 document.getElementById("contactForm").addEventListener("submit", sendmail);
-
-function sendmail(){
-    var params ={
-      
-        message : document.getElementById("message").value,
-        email_id : document.getElementById("email").value,
-        services : document.getElementById("services").value,
-        name : document.getElementById("name").value,
-    };
-    emailjs.send("service_b52pvoc","template_lhu5kvc", params)
-    .then(
-       function(res){
-        alert("success"+ res.status);
-       }
-    )
-    .catch((err)=> console.log("there is error"));
-}
-
